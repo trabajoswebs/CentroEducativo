30307c43d3048c7a42fd93552419758867e18432
Johan Manuel Gonzalez <gonzalezjoan@hotmail.com>
2020-03-11 18:02:24 +0100

    Correción y ubicacion de metodo cargaDatos

--- src/profesorampliado/TablasCursos.java
+++ src/profesorampliado/TablasCursos.java
@@ -5,6 +5,10 @@
  */
 package profesorampliado;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.TreeMap;
 
 /**
@@ -12,43 +16,63 @@ import java.util.TreeMap;
  * @author Acer
  */
 public class TablasCursos {
+    public static final String CAFilePath = "C:\\Users\\Acer\\Documents\\Formacion Profesional Desarrollo Aplicaciones Web\\Practica java\\Practicas Personas\\centroeducativo\\Profesores\\CursoAsignatura.txt";
     
-    public static final String directory = "C:\\Users\\Acer\\Documents\\Formacion Profesional Desarrollo Aplicaciones Web\\Practica java\\Practicas Personas\\profesoresyAlumnos\\Profesores\\";
     
     static void cargaCursos(TreeMap<String, String> tmCC){
-        tmCC.put("1E", "SMR Mañana");
-        tmCC.put("1G", "SMR Tarde");
-        tmCC.put("1F", "SMR Tarde");
-        tmCC.put("1W", "DAW Tarde");
-        tmCC.put("1S", "DAW Mañana");
+        cargaDatos(Cursos.filePath, CentroEducativo.tmCC);
     }
+    
+    
     static void cargaCursosAsignaturas(TreeMap<String, String> tmCCASIGNA){
-        tmCCASIGNA.put("1EREDES", "Redes de Área Local");
-        tmCCASIGNA.put("1EOFIMA", "Ofimática");
-        tmCCASIGNA.put("1ESISOP", "Sistemas Operativos");
-        tmCCASIGNA.put("1EFOL", "Formación y Orientación Laboral");
-        tmCCASIGNA.put("1EINGLES", "Inglés");
-        tmCCASIGNA.put("1GREDES", "Redes de Área Local");
-        tmCCASIGNA.put("1GOFIMA", "Ofimática");
-        tmCCASIGNA.put("1GSISOP", "Sistemas Operativos");
-        tmCCASIGNA.put("1GFOL", "Formación y Orientación Laboral");
-        tmCCASIGNA.put("1FREDES", "Redes de Área Local");
-        tmCCASIGNA.put("1FOFIMA", "Ofimática");
-        tmCCASIGNA.put("1FSISOP", "Sistemas Operativos");
-        tmCCASIGNA.put("1FFOL", "Formación y Orientación Laboral");
-        tmCCASIGNA.put("1FINGLES", "Inglés");
-        tmCCASIGNA.put("1WPROGRAM", "Programación");
-        tmCCASIGNA.put("1WLENGMAR", "Lenguajes de Marcas");
-        tmCCASIGNA.put("1WENTORNOS", "Entornos de Desarrollo");
-        tmCCASIGNA.put("1WSISOPE", "Sistemas Operativos");
-        tmCCASIGNA.put("1WFOL", "Formación y Orientación Laboral");
-        tmCCASIGNA.put("1WINGLES", "Inlés");
-        tmCCASIGNA.put("1SPROGRAM", "Programación");
-        tmCCASIGNA.put("1SLENGMAR", "Lenguajes de Marcas");
-        tmCCASIGNA.put("1SENTORNOS", "Entornos de Desarrollo");
-        tmCCASIGNA.put("1SSISOPE", "Sistemas Operativos");
-        tmCCASIGNA.put("1SFOL", "Formación y Orientación Laboral");
-        tmCCASIGNA.put("1SINGLES", "Inlés");
+        cargaDatos(CAFilePath, CentroEducativo.tmCCASIGNA);
+    }
+    
+    /**
+     * Lee los datos que se necuentra en el fichero y los carga en el treeMap
+     */
+    public static void cargaDatos(String filePath, TreeMap<String, String> tm){
+        FileReader fr = null;
+        BufferedReader entrada = null;
+        int indice = 0;
+        String cadena, key, value;
+        
+        try {
+            fr = new FileReader(filePath);
+            entrada = new BufferedReader(fr);
+            
+            cadena = entrada.readLine();
+            
+            while(cadena != null){
+                indice = cadena.indexOf(",");
+                if (indice != -1) {
+                    key = cadena.substring(0, indice).toUpperCase();
+                    value = cadena.substring(indice + 1 ).toUpperCase();
+                    tm.put(key, value);
+                }
+                cadena = entrada.readLine();
+            }
+        }
+        catch (FileNotFoundException fnf) {
+            fnf.printStackTrace();
+        }
+        catch (IOException ioe) {
+            System.out.println("Ha ocurrido una excepción: " + ioe.getMessage());    
+        }
+        catch (Exception e) {
+            System.out.println("Ha ocurrido una excepción: " + e.getMessage());   
+        }finally{
+            try {
+                if (fr != null) {
+                    fr.close();
+                }
+                if(entrada != null){
+                    entrada.close();
+                }
+            } catch (IOException e) {
+                System.out.println("Se ha producido un error al intentar cerrar el fichero: " + e.getMessage());
+            }
+        }
     }
 }
 
