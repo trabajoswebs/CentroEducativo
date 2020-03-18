e48da9f13219741a4c5d54fb3a08242f1e547571
Johan Manuel <Johan Manuel@DESKTOP-I06SU2E>
2020-03-18 16:23:02 +0100

    cambios y correcion de errores

--- src/centroEducativo/Persona.java
+++ src/centroEducativo/Persona.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package profesorampliado;
+package centroEducativo;
 
 import static java.lang.Integer.parseInt;
 import java.util.Scanner;
